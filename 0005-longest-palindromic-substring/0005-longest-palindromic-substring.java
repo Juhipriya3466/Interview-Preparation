8e0d244f567a409fb9af614ca8ffdb59f299aac9
Juhipriya3466 <68154740+Juhipriya3466@users.noreply.github.com>
2022-10-16 11:52:04 +0530

    Time: 36 ms (64.45%), Space: 42 MB (96.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,5 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) {
+        //Using Expand around center technique
+        int len=1;
+        int start=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int low=i-1;
+            int high=i+1;
+            while(low>=0 && s.charAt(low)==s.charAt(i))
+                low--;
+            while(high<s.length() && s.charAt(high)==s.charAt(i))
+                high++;
+            while(low>=0 && high<s.length() && s.charAt(low)==s.charAt(high))
+            {
+                low--;
+                high++;
+            }
+            if(len<high-low-1)
+            {
+            len=high-low-1;
+            start=low+1;
+            }  
+        }
+        return s.substring(start,start+len);
+    }
+}
+        //Using Dynamic Programming
+        /*
         int[][] dp=new int[s.length()][s.length()];
         for(int i=0;i<dp.length;i++)
             Arrays.fill(dp[i],-1);
@@ -31,5 +58,5 @@ class Solution {
         int ans=((s.charAt(i)==s.charAt(j))?1:0) & isPalindrome(i+1,j-1,s,dp);
         dp[i][j]=ans;
         return ans;
-    }
-}
+    }*/
+        
